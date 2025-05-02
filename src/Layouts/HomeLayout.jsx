335054da9d532c12775f53ac28cce60331208c95
sohail4612 <sohailshaikh4612@gmail.com>
2025-05-02 16:09:04 +0600

    categories part added

---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from '../Components/Header';
+import { Outlet } from 'react-router';
+import LatestNews from '../Components/LatestNews';
+import Navbar from '../Components/Navbar';
+import LeftAside from '../Components/Home Layout/LeftAside';
+import RightAside from '../Components/Home Layout/RightAside';
+
+const HomeLayout = () => {
+    return (
+        <div>
+           <header>
+            <Header></Header>
+            <section className='w-11/12 mx-auto my-3'>
+                <LatestNews></LatestNews>
+            </section>
+            <nav className='w-11/12 mx-auto my-3'>
+                <Navbar></Navbar>
+            </nav>
+           </header>
+           <main className='w-11/12 mx-auto my-3 grid grid-cols-12'>
+            <aside className='col-span-3'>
+                <LeftAside></LeftAside>
+            </aside>
+            <section className="main col-span-6">
+                <Outlet />
+            </section>
+            <aside className='col-span-3'>
+                <RightAside></RightAside>
+            </aside>
+           </main>
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layouts/HomeLayout";
+import Home from "../pages/Home";
+import CategoryNews from "../pages/CategoryNews";
+
+
+const Router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            Component: HomeLayout,
+            children: [
+                {
+                    path:"",
+                    element: <Home></Home>
+                },
+
+                {
+                    path:"/category/:id",
+                    element: <CategoryNews></CategoryNews>
+                },
+                {
+                    path:"",
+                    element: <Home></Home>
+                }
+            ]
+        },
+        {
+            path: 'auth',
+            element: <h2>authentication</h2>
+        },
+        {
+            path: 'news',
+            element: <h2>news</h2>
+        },
+        {
+            path: '*',
+            element: <h2>error</h2>
+        }
+    ]
+);
+export default Router
